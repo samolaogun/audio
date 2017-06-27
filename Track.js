dac60d6d37c90afa7e7806b12502be3b02f5cc3d
Sam Olaogun <sam.olaogun@gmail.com>
2017-06-27 13:24:39 -0400

    Update Track.js

--- Track.js
+++ Track.js
@@ -1,77 +1,54 @@
 'use strict';
 
-import { context, main } from './App.js';
 import Source from './Source.js';
 
-// Create wave from buffers, layer (unary or) when necessary
-// do wave buffer in wasm and javascrip typed array
-
 export default class Track {
-    constructor() {
-        // Consolidate nodes to one gain node
-        this.context = new AudioContext();
-
-        this.gainNode = this.context.createGain();
-        this.gainNode.gain.value = .5;
-        this.gainNode.connect(this.context);
-
-        this.context.connect(context.destination);
-
-        this.endTime = 0;
+    constructor(container) {
+        this.totalTime = 0;
         this.pixelSecondRatio = 1;
         this.sources = [];
 
         this.element = document.createElement('div');
         this.element.classList.add('track');
-        main.insertAdjacentElement('beforeend', this.element);
+        container.insertAdjacentElement('beforeend', this.element);
 
-        this.element.addEventListener('drop', async e => {
-            e.preventDefault();
+        this.element.addEventListener('drop', this._handleDrop);
+    }
+    
+    async _handleDrop(e) {
+        e.preventDefault();
 
-            if (!e.dataTransfer)
-                return;
+        if (!e.dataTransfer) return;
 
-            const file = await (async() => {
-                const [item, ] = e.dataTransfer.items;
-                const entry = item.webkitGetAsEntry();
+        const [item, ] = e.dataTransfer.items;
+        const entry = item.webkitGetAsEntry();
+        
+        const file = await new Promise((...handlers) => { entry.file(...handlers); });
+        
+        if (!(/^audio/.test(file.type))) return;
 
-                return await new Promise((...handlers) => { entry.file(...handlers); });
-            })();
+        const source = context.createBufferSource();
 
-            if (!(/^audio/.test(file.type)))
-                return invalidAudio();
+        source.buffer = await new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
 
-            const source = context.createBufferSource();
+            fileReader.addEventListener('load', () => context.decodeAudioData(fileReader.result, resolve, reject));
 
-            source.buffer = await new Promise((resolve, reject) => {
-                const fileReader = new FileReader();
-
-                fileReader.addEventListener('load', () => {
-                    context.decodeAudioData(fileReader.result, resolve, reject);
-                });
-
-                fileReader.addEventListener('error', reject);
-                fileReader.readAsArrayBuffer(file);
-            });
-
-            source.connect(context.destination);
-
-            // Changed from buffer to source buffer
-            this.addSource(source, source.buffer.duration, file.name);
+            fileReader.addEventListener('error', reject);
+            fileReader.readAsArrayBuffer(file);
         });
+
+        source.connect(context.destination);
+
+        this.addSource(file, source);
     }
 
     start() {
-        // On drag, change offset
-        this.sources.forEach(source => {
-            // Play forever or stop at the end?
-            source.start();
-        });
+        this.sources.forEach(source => source.start());
     }
 
-    addSource(source, duration, name) {
-        // Tip: Use start and stop for layering
-        this.sources.push(new Source(this, this.gainNode, duration, this.endTime, name));
-        this.endTime += duration;
+    addSource({name}, {duration}) { 
+        this.sources.push(new Source(this, { duration, name }));
+        this.totalTime += duration;
     }
 }
