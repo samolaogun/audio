efcff6d9057c15a5a6d1a15a4287a771d3ba3fa9
Sam Olaogun <sam.olaogun@gmail.com>
2017-06-27 13:28:05 -0400

    Update Source.js

--- Source.js
+++ Source.js
@@ -2,30 +2,23 @@
 
 export default class Source {
     constructor(track, { duration, name }) {
-        this.name = name;
-        
-        this.duration = duration;
-        this.track= track;
+        this.track = track;
         this.offset = track.offset;
-
-        // Append to back of track by default
-        this.startTime = track.endTime;
+        
+        this.name = name;
+        this.duration = duration;
+        this.time = track.totalTime;
 
         this.element = document.createElement('div');
         this.element.classList.add('track__source');
         this.element.style.width = `${duration}px`;
-        this.element.style.maxWidth = `${duration}px`;
-        // Linear gradient
-        this.element.style.backgroundColor = this.color;
-
-        // Add more performant version (FLIP) later
-        // this.element.style.transform = t`translateX(${track.endTime}px)`;
-
-        this.element.addEventListener('drag', function() {
-            // Perform cross track things and in rearrangements
-        });
-
         track.element.insertAdjacentElement('beforeend', this.element);
+        
+        this.element.addEventListener('drag', this._handleDrag);
+    }
+    
+    async _handleDrag () {
+    
     }
 
     start() {
